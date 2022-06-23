daf851fd58dd4ea304d3f0d4487eccf8c3e50722
Aingareng <ngarengai@gmail.com>
2022-06-23 13:40:32 +0800

    Update tugas index.js

--- build/index.js
+++ build/index.js
@@ -1,2 +1,31 @@
 "use strict";
-console.log("hai world");
+class Motorcycle {
+    constructor(name, release, color) {
+        this.name = name;
+        this.release = release;
+        this.color = color;
+    }
+    vehicleType() {
+        console.log(`Merek Kendaraan : ${this.name}, Waktu rilis : ${this.release}, Warna Kendaraan : ${this.color}`);
+    }
+}
+class Car extends Motorcycle {
+    constructor(name, release, color) {
+        super(name, release, color);
+    }
+}
+const Beat = new Motorcycle("Beat", 2010, "read");
+const Fortuner = new Car("Forutner", 2015, "white");
+Beat.vehicleType();
+Fortuner.vehicleType();
+// 3. looping data array
+let users;
+users = [
+    { name: 'John', age: 20, job: 'Writer' },
+    { name: 'Doe', age: 22, job: 'Writer' },
+    { name: 'Don', age: 21, job: 'Coder' },
+];
+users.forEach((value) => {
+    const { name, age, job } = value;
+    console.log(`${name} : ${age} : ${job}`);
+});
